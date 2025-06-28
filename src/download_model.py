3f764e63a1d00efb10c162e16b93344036d82d1a
MiskirB <miskerbab@gmail.com>
2025-06-29 02:52:49 +0300

    Add downloaded MLflow model for Render deployment

--- src/download_model.py
+++ src/download_model.py
@@ -1,8 +1,7 @@
-import mlflow.artifacts
+import mlflow
 
 model_uri = "models:/CreditRiskLogisticModel/1"
 local_path = "model"
 
-mlflow.artifacts.download_artifacts(artifact_uri=model_uri, dst_path=local_path)
-
-print(f"✅ Model downloaded to: {local_path}")
+local_dir = mlflow.artifacts.download_artifacts(artifact_uri=model_uri, dst_path=local_path)
+print(f"✅ Model downloaded to: {local_dir}")
